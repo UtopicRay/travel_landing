ad26a62efaf4f37fa4b2527765db344828e13782
UtopicRay <rayrm@gmail.com>
2024-11-14 00:00:16 -0500

    'Create Navbar and Button Component'

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type: "button"|"submit";
+  label: string;
+  icon?: string;
+  variant: string;
+};
+
+export default function Button({ label, icon, variant,type }: ButtonProps) {
+  return (
+    <button type={type} className={`flexCenter gap-3 rounded-full ${variant}`}>
+      {icon ? <Image src={icon} alt="icon" width={26} height={26}></Image> : ""}
+      <label>{label}</label>
+    </button>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,40 @@
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="padding-container max-container flexBetween py-5 relative z-30">
+      <Link href="/">
+        <Image src="/img/hilink-logo.svg" alt="hilink" width={80} height={80} />
+      </Link>
+      <ul className="hidden lg:flex gap-12">
+        {NAV_LINKS.map((link) => (
+          <Link
+            className="regular-16 text-gray-50 font-semibold transition-all hover:font-bold hover:duration-150"
+            href={link.href}
+            key={link.key}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="hidden lg:flexCenter">
+        <Button
+          label="login"
+          type="button"
+          variant="btn_dark_green"
+          icon="/img/user.svg"
+        ></Button>
+      </div>
+      <Image
+        className="flex lg:hidden"
+        src="/img/menu.svg"
+        alt="menu_icon"
+        width={36}
+        height={36}
+      ></Image>
+    </nav>
+  );
 }
