057cc09a77fbfc5f428c08c0aca821b29cd34bda
UtopicRay <rayrm@gmail.com>
2024-11-17 22:18:02 -0500

    Add Animation to CampSection

--- src/components/CampSection.tsx
+++ src/components/CampSection.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Camp from "./Camp";
 
 export default function CampSection() {
@@ -19,17 +18,15 @@ export default function CampSection() {
         ></Camp>
       </div>
       <div className="flexEnd px-6">
-        <div className="w-full mt-12 bg-green-50 lg:max-w-[200px] 2xl:max-w-[768px] p-6 rounded-3xl xl:rounded-5xl lg:mt-[-200px] z-30">
+        <div className="w-full fade-up mt-12 bg-green-50 lg:max-w-[200px] 2xl:max-w-[734px] p-6 rounded-3xl xl:rounded-5xl lg:mt-[-200px] z-30">
           <h2 className="text-white regular-24 md:regular-32 xl:regular-64">
-            <strong>Lorem ipsum dolor sit amet</strong> consectetur adipisicing
-            elit. La?
+            <strong>Feeling Lost</strong> And Not Knowing The Way?
           </h2>
           <p className="regular-16 text-white lg:regular-16 mt-6">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-            maiores, ea quisquam debitis aliquam sequi molestias blanditiis
-            molestiae quam assumenda illo consectetur! Numquam, voluptatum sed.
-            Ratione, facilis necessitatibus aperiam velit, numquam deleniti
-            tenetur voluptas perspiciatis facere, fuga odio amet praesentium?
+            Starting from the anxiety of the climbers when visiting a new
+            climbing location, the possibility of getting lost is very large.
+            That's why we are here for those of you who want to start an
+            adventure
           </p>
         </div>
       </div>
